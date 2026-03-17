589552279e57aadbb9e1f34288d68cf961c8ee73
Ron Avriel <ron.avriel@audiocodes.com>
2026-03-17 18:17:06 +0200

    Revert "Fix RMQ event handlers not removed (#4044)"
    This reverts commit 1208c5b17146e0aea43a63c22209b65977b019d0.

--- src/Paramore.Brighter.MessagingGateway.RMQ.Async/RmqMessageProducer.cs
+++ src/Paramore.Brighter.MessagingGateway.RMQ.Async/RmqMessageProducer.cs
@@ -151,9 +151,9 @@ public partial class RmqMessageProducer : RmqMessageGateway, IAmAMessageProducer
             var rmqMessagePublisher = new RmqMessagePublisher(Channel, Connection);
 
             message.Persist = Connection.PersistMessages;
+            Channel.BasicAcksAsync += OnPublishSucceeded;
+            Channel.BasicNacksAsync += OnPublishFailed;
             
-            RegisterChannelEvents(true);
-
             BrighterTracer.WriteProducerEvent(Span, MessagingSystem.RabbitMQ, message, _instrumentationOptions);
 
             Log.PublishingMessageAsync(s_logger, Connection.Exchange.Name, Connection.AmpqUri.GetSanitizedUri(), delay.Value.TotalMilliseconds,
@@ -188,20 +188,6 @@ public partial class RmqMessageProducer : RmqMessageGateway, IAmAMessageProducer
         }
     }
 
-    private void RegisterChannelEvents(bool add)
-    {
-        if (add)
-        {
-            Channel?.BasicAcksAsync += OnPublishSucceeded;
-            Channel?.BasicNacksAsync += OnPublishFailed;
-        }
-        else
-        {
-            Channel?.BasicAcksAsync -= OnPublishSucceeded;
-            Channel?.BasicNacksAsync -= OnPublishFailed;
-        }
-    }
-
     public sealed override void Dispose()
     {
         GC.SuppressFinalize(this);
@@ -209,7 +195,6 @@ public partial class RmqMessageProducer : RmqMessageGateway, IAmAMessageProducer
 
     private Task OnPublishFailed(object sender, BasicNackEventArgs e)
     {
-        RegisterChannelEvents(false);
         if (_pendingConfirmations.TryGetValue(e.DeliveryTag, out var messageId))
         {
             OnMessagePublished?.Invoke(false, messageId);
@@ -222,7 +207,6 @@ public partial class RmqMessageProducer : RmqMessageGateway, IAmAMessageProducer
 
     private Task OnPublishSucceeded(object sender, BasicAckEventArgs e)
     {
-        RegisterChannelEvents(false);
         if (_pendingConfirmations.TryGetValue(e.DeliveryTag, out var messageId))
         {
             OnMessagePublished?.Invoke(true, messageId);
